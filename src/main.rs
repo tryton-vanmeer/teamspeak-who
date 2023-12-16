7ac272d857e0af3776df0346859a8e2882f2fb21
Tryton Van Meer <code@trytonvanmeer.dev>
2023-12-16 15:01:00 -0500

    implement main using teamspeak and config modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod teamspeak;
+
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let config = config::load_config()?;
+
+    let mut connection = teamspeak::connect(config.server, config.password).await?;
+
+    let state = connection.get_state()?;
+
+    let &channel = state
+        .channels
+        .iter()
+        .find_map(|(key, val)| {
+            if val.name == config.channel {
+                Some(key)
+            } else {
+                None
+            }
+        })
+        .unwrap();
+
+    for client in state.clients.values() {
+        if client.channel == channel {
+            println!("{}", client.name);
+        }
+    }
+
+    teamspeak::disconnect(&mut connection).await;
+
+    Ok(())
 }
