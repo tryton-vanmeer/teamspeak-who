edcb018978b6ef147566f3faa2329a9d9d3c2927
Tryton Van Meer <code@trytonvanmeer.dev>
2023-12-16 14:36:49 -0500

    add tsclientlib and implement connect wrapper function

---
+++ src/teamspeak.rs
@@ -0,0 +1,36 @@
+use anyhow::{Result, bail};
+use futures::prelude::*;
+use tokio::time::{self, Duration};
+use tsclientlib::{Connection, OutCommandExt, StreamItem};
+
+pub async fn connect(address: String, password: String) -> Result<Connection> {
+    let mut connection = Connection::build(address)
+        .version(tsclientlib::Version::Linux_3_X_X)
+        .password(password)
+        .name("teamspeak-who")
+        .connect()?;
+
+    connection
+        .events()
+        .try_filter(|e| future::ready(matches!(e, StreamItem::BookEvents(_))))
+        .next()
+        .await
+        .unwrap()?;
+
+    connection
+        .get_state()?
+        .server
+        .set_subscribed(true)
+        .send(&mut connection)?;
+
+    let mut events = connection.events().try_filter(|_| future::ready(false));
+    tokio::select! {
+        _ = time::sleep(Duration::from_secs(1)) => {}
+        _ = events.next() => {
+            bail!("Disconnected");
+        }
+    };
+    drop(events);
+
+    Ok(connection)
+}
