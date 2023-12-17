6ed24ebd1dbf49b55cf67fa75abf36a18460e0b3
Tryton Van Meer <code@trytonvanmeer.dev>
2023-12-16 21:30:22 -0500

    implement stuct wrapping the ts connection

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ use anyhow::Result;
 async fn main() -> Result<()> {
     let config = config::load_config()?;
 
-    let mut connection = teamspeak::connect(config.server, config.password).await?;
+    let mut client = teamspeak::Client::init(config.server, config.password).await?;
 
-    let state = connection.get_state()?;
+    // let state = connection.get_state()?;
 
-    for client in state.clients.values() {
-        println!("{}", client.name);
-    }
+    // for client in state.clients.values() {
+    //     println!("{}", client.name);
+    // }
 
-    teamspeak::disconnect(&mut connection).await;
+    client.disconnect().await;
 
     Ok(())
 }
--- src/teamspeak.rs
+++ src/teamspeak.rs
@@ -1,41 +1,52 @@
 use anyhow::{bail, Result};
 use futures::prelude::*;
 use tokio::time::{self, Duration};
-use tsclientlib::{Connection, OutCommandExt, StreamItem, DisconnectOptions};
+use tsclientlib::{Connection, DisconnectOptions, OutCommandExt, StreamItem};
 
-pub async fn connect(address: String, password: String) -> Result<Connection> {
-    let mut connection = Connection::build(address)
-        .version(tsclientlib::Version::Linux_3_X_X)
-        .password(password)
-        .name("teamspeak-who")
-        .connect()?;
-
-    connection
-        .events()
-        .try_filter(|e| future::ready(matches!(e, StreamItem::BookEvents(_))))
-        .next()
-        .await
-        .unwrap()?;
-
-    connection
-        .get_state()?
-        .server
-        .set_subscribed(true)
-        .send(&mut connection)?;
-
-    let mut events = connection.events().try_filter(|_| future::ready(false));
-    tokio::select! {
-        _ = time::sleep(Duration::from_secs(1)) => {}
-        _ = events.next() => {
-            bail!("Disconnected");
-        }
-    };
-    drop(events);
-
-    Ok(connection)
+pub struct Client {
+    connection: Connection,
 }
 
-pub async fn disconnect(connection: &mut Connection) {
-    connection.disconnect(DisconnectOptions::new()).unwrap();
-    connection.events().for_each(|_| future::ready(())).await;
+impl Client {
+    pub async fn init(address: String, password: String) -> Result<Client> {
+        let mut connection = Connection::build(address)
+            .version(tsclientlib::Version::Linux_3_X_X)
+            .password(password)
+            .name("teamspeak-who")
+            .connect()?;
+
+        connection
+            .events()
+            .try_filter(|e| future::ready(matches!(e, StreamItem::BookEvents(_))))
+            .next()
+            .await
+            .unwrap()?;
+
+        connection
+            .get_state()?
+            .server
+            .set_subscribed(true)
+            .send(&mut connection)?;
+
+        let mut events = connection.events().try_filter(|_| future::ready(false));
+        tokio::select! {
+            _ = time::sleep(Duration::from_secs(1)) => {}
+            _ = events.next() => {
+                bail!("Disconnected");
+            }
+        };
+        drop(events);
+
+        Ok(Client { connection })
+    }
+
+    pub async fn disconnect(&mut self) {
+        self.connection
+            .disconnect(DisconnectOptions::new())
+            .unwrap();
+        self.connection
+            .events()
+            .for_each(|_| future::ready(()))
+            .await;
+    }
 }
